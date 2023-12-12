1be6aa2eea6e8a98bba9375409c5c2b973781418
shafirahalma <halmaherashafira@Gmail.com>
2023-12-12 07:53:03 +0700

    init

--- adminpages/fungsi/config.php
+++ adminpages/fungsi/config.php
@@ -1,6 +1,5 @@
 <?php
-$conn = new mysqli('localhost', 'root', '', 'kue');
-if($conn->connect_error){
-	die("Fatal Error: Can't connect to database: ". $conn->connect_error);
+$conn = new mysqli('localhost', 'root', '', 'bakery');
+if ($conn->connect_error) {
+	die("Fatal Error: Can't connect to database: " . $conn->connect_error);
 }
-?>
